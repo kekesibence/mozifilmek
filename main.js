3d6c67fb03e6010efc8b33066dd605c667fe44e9
kekesibence <48643274+kekesibence@users.noreply.github.com>
2021-10-05 15:46:25 +0200

    Construktor atirasa

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 class Film {
-    constructor(cim, hossz) {
+    constructor(cim, rendezo, kiadasiEv) {
         if (typeof this.cim == 'string') { this.cim = cim;}
         else { document.getElementById("hibaCim") = "Nem jó formátum"; }
-        if (typeof this.hossz == 'number') {this.hossz = hossz;}
-        else { document.getElementById("hibaHossz") = "Nem jó formátum"; }
+        if (typeof this.rendezo == 'string') { this.rendezo = rendezo;}
+        else { document.getElementById("hibaRendezo") = "Nem jó formátum"; }
+        if (typeof this.kiadasiEv == 'number') {this.kiadasiEv = kiadasiEv;}
+        else { document.getElementById("hibaKiadasiEv") = "Nem jó formátum"; }
     }
     toString() {
-        return this.cim + "( " + this.hossz + " perc)";
+        return this.cim + " " + this.rendezo+ " " +this.kiadasiEv;
     }
     elkuldes() 
 }
